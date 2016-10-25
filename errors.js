0a21b02e9829efdd2b483009b7a2cb94427ecae5
Steve Lacy <me@slacy.me>
2016-10-25 16:04:16 -0700

    Add DataRequestError to differentiate between network and data not found errors

--- errors.js
+++ errors.js
@@ -2,3 +2,4 @@ var makeErrorCause = require('make-error-cause')
 
 exports.RangeNotFoundError = makeErrorCause('RangeNotFoundError')
 exports.DataNotFoundError = makeErrorCause('DataNotFoundError')
+exports.DataRequestError = makeErrorCause('DataRequestError')
